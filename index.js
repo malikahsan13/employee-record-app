b5d6658bd364a1b4f5b6dafa5bb1d08557a11acf
MalikAhsan-PC <malikahsan13@gmail.com>
2023-11-18 18:03:43 +0500

    added code for department apis CRUD operations

--- index.js
+++ index.js
@@ -29,3 +29,39 @@ app.listen(5000, () => {
 app.get("/", (req, res) => {
   res.send("Welcome to Employees Database");
 });
+
+app.get("/api/department", (req, res) => {
+  const query = `SELECT * FROM department`;
+  connection.query(query, function (error, rows, fields) {
+    if (error) res.send(`Error fetching data ${error}`);
+    res.send(rows);
+  });
+});
+
+app.post("/api/department", (req, res) => {
+  const query = `INSERT INTO department (name) VALUE(?);`;
+  let values = [req.body["name"]];
+
+  connection.query(query, values, function (err, rows, fields) {
+    if (err) res.send("Error inserting data");
+    res.json("Successful");
+  });
+});
+
+app.put("/api/department", (req, res) => {
+  const query = `UPDATE department SET name=? WHERE id=?`;
+  let values = [req.body["name"], [req.body["id"]]];
+  connection.query(query, values, function (err, rows, fields) {
+    if (err) res.send("Error updating Department");
+    res.json("Successfull update");
+  });
+});
+
+app.delete("/api/department", (req, res) => {
+  const query = `DELETE FROM department WHERE id=?`;
+  let values = [req.body["id"]];
+  connection.query(query, values, function (err, rows, fields) {
+    if (err) res.send("Error deleting department");
+    res.json("Successfully Deleted");
+  });
+});

896cbbc7fb141bbf434e3ddee99e2ae488f184b3
MalikAhsan-PC <malikahsan13@gmail.com>
2023-11-18 17:03:14 +0500

    added mysql package and added code for connecting to mysql database

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql");
 
 const app = express();
 
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.listen(5000, (req, res) => {
-  res.send("Hello World");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "department-db",
+});
+
+// console.log(connection);
+
+app.listen(5000, () => {
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to DB");
+  });
+  console.log("Server listening on Port 5000");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Employees Database");
 });

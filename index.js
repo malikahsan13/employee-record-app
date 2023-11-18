be3dc8ddf6b9f6b04447d18b3df6ac2f1cb66f70
MalikAhsan-PC <malikahsan13@gmail.com>
2023-11-18 13:40:08 +0500

    added cors package to allow other domains to access api endpoints

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyparser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(5000, (req, res) => {
   res.send("Hello World");

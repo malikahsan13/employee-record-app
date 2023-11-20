47e7190ee59117396481053492e8f83bf211105d
MalikAhsan-PC <malikahsan13@gmail.com>
2023-11-21 00:31:16 +0500

    added react routes using react router dom

--- frontend/employee-rec-app/src/App.js
+++ frontend/employee-rec-app/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Home";
+import Employee from "./Employee";
+import Department from "./Department";
+import { BrowserRouter, Route, Routes, NavLink } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h3 className="d-flex justify-content-center m-3">React JS Frontend</h3>
+      <BrowserRouter>
+        <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+          <ul className="navbar-nav">
+            <li className="nav-item m-1">
+              <NavLink className="btn btn=light btn-outline-primary" to="/home">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/department"
+              >
+                Department
+              </NavLink>
+            </li>
+            <li className="nav-item m-1">
+              <NavLink
+                className="btn btn-light btn-outline-primary"
+                to="/employee"
+              >
+                Employee
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/employee" exact element={<Employee />} />
+          <Route path="/department" exact element={<Department />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

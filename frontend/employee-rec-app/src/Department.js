c3d5e1bb70d37d9c034235ccc9c85b47ca4ed897
MalikAhsan-PC <malikahsan13@gmail.com>
2023-11-21 01:26:20 +0500

    created department component user interface and fetched data from department api

--- frontend/employee-rec-app/src/Department.js
+++ frontend/employee-rec-app/src/Department.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { variables } from "./Variables";
 
 const Department = () => {
-  return <div>Department</div>;
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let res = await fetch(variables.API_URL + "department", {
+        method: "GET",
+      });
+      if (res.ok) {
+        let resBody = await res.json();
+        setDepartments(resBody);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <table className="table table-stripped">
+        <thead>
+          <tr>
+            <th scope="col">Department ID</th>
+            <th scope="col">Department Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {departments.map((dept) => {
+            return (
+              <tr key={dept.id}>
+                <td>{dept.id}</td>
+                <td>{dept.name}</td>
+                <td>
+                  <i class="bi bi-pencil-square"></i>
+                  <i class="bi bi-trash"></i>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Department;
